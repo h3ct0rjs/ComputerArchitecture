9e210deff04574ea040ad8514d603c00d31c5120
carlos <carlos.23325@gmail.com>
2017-10-22 21:48:16 -0500

    se agrega el seu que completa a 31

--- Processor/Entrega3/Seu.vhd
+++ Processor/Entrega3/Seu.vhd
@@ -1,11 +1,3 @@
--- Create Date:    16:41:40 09/28/2017 
--- Design Name: 
--- Module Name:    Mod5Seu - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -21,12 +13,11 @@ architecture Behavioral of Mod5Seu is
 
 begin
 	process (imm13)
-	begin 
+	begin
 		if imm13(12) = '1' then
-			SEUimm32 <= "1111111111111111111"&imm13;
-		else 
-			SEUimm32<= "0000000000000000000"&imm13;
+			SEUimm32 <= "1111111111111111111" & imm13;
+		else
+			SEUimm32 <= "0000000000000000000" & imm13;
 		end if;
 	end process;
-	
 end Behavioral;
