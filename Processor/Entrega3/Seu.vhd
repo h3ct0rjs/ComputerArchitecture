52927c99f546b531f805699f46c4a6593fa05668
carlos <carlos.23325@gmail.com>
2017-10-22 21:50:13 -0500

    tabs by spaces :D

--- Processor/Entrega3/Seu.vhd
+++ Processor/Entrega3/Seu.vhd
@@ -2,22 +2,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
-
 entity Mod5Seu is
     Port ( imm13 : in  STD_LOGIC_VECTOR (12 downto 0);
-           SEUimm32 : out  STD_LOGIC_VECTOR (31 downto 0));
+           SEUimm32 : out  STD_LOGIC_VECTOR (31 downto 0)
+         );
 end Mod5Seu;
 
 architecture Behavioral of Mod5Seu is
 
 begin
-	process (imm13)
-	begin
-		if imm13(12) = '1' then
-			SEUimm32 <= "1111111111111111111" & imm13;
-		else
-			SEUimm32 <= "0000000000000000000" & imm13;
-		end if;
-	end process;
+  process (imm13)
+  begin
+    if imm13(12) = '1' then
+      SEUimm32 <= "1111111111111111111" & imm13;
+    else
+      SEUimm32 <= "0000000000000000000" & imm13;
+    end if;
+  end process;
 end Behavioral;
